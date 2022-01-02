331c7dc6c4751bdfb9eff0dd48911e113f66513a
Paweł <pawel.pycia29@gmail.com>
2022-01-02 16:00:05 +0100

    Create P2.py

---
+++ P2.py
@@ -0,0 +1,13 @@
+from typing import List
+
+from Graph import Graph
+from Production import Production
+from Vertex import Vertex
+
+
+class P2(Production):
+    @staticmethod
+    def apply(vertices: List[Vertex], graph: Graph):
+        from_vertex = vertices[0]
+        to_vertex = vertices[1]
+        del graph.edges[(from_vertex.index, to_vertex.index)]
