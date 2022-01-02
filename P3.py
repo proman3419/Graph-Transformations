a8ceb77f47f5966f8c5ca1ab96c42bee236564e1
Paweł <pawel.pycia29@gmail.com>
2022-01-02 16:58:40 +0100

    fixing P3

--- P3.py
+++ P3.py
@@ -9,7 +9,7 @@ class P3(Production):
     @staticmethod
     def apply(vertices: List[Vertex], graph: Graph):
         new_vertex = graph.add_vertex('F')
-      
-        for vertex in vertices:
+        graph.add_edge('d', vertices[0], new_vertex)
+        for vertex in graph.vertices.values():
             if vertex.label == 'Z':
                 graph.add_edge('d', vertex, new_vertex)
