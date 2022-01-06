3cb66067622da65173b04c995434b9a841de3ec5
maati01 <mateuszpow7@gmail.com>
2022-01-06 15:13:07 +0100

    nothing important

--- Visualization.py
+++ Visualization.py
@@ -6,11 +6,12 @@ from bokeh.plotting import figure, from_networkx
 
 class Visualization:
     def __init__(self, graph):
+        self.graph = graph
         self.button = Button(label="Click on the button",
                         button_type="danger")
         self._create_buttons()
 
-        self.G = nx.Graph()
+        self.graph = self._convert_graph(self.graph)
 
         self.plot = figure(width=800, height=600, x_range=(-1.2, 1.2), y_range=(-1.2, 1.2),
                       x_axis_location=None, y_axis_location=None, toolbar_location=None,
@@ -18,19 +19,36 @@ class Visualization:
                       tooltips="index: @index, label: @label")
         self.plot.grid.grid_line_color = None
 
-        graph_renderer = from_networkx(self._convert_graph(graph), nx.spring_layout, scale=1, center=(0, 0))
-        graph_renderer.node_renderer.glyph = Circle(size=15, fill_color="lightblue")
-        graph_renderer.edge_renderer.glyph = MultiLine(line_color="black",
+        self.graph_renderer = from_networkx(self.graph, nx.spring_layout, scale=1, center=(0, 0))
+        self.graph_renderer.node_renderer.glyph = Circle(size=15, fill_color="lightblue")
+        self.graph_renderer.edge_renderer.glyph = MultiLine(line_color="black",
                                                        line_alpha=0.8, line_width=1.5)
 
-        self.plot.renderers.append(graph_renderer)
-
+        self.plot.renderers.append(self.graph_renderer)
 
         self.layout = column(self.button, self.plot)
 
     def cos(self):
-        self.G.add_node(4)
-        save(self.layout)
+        self.graph.add_node(4)
+        self.graph.add_edge(3, 4)
+
+        self.plot = figure(width=800, height=600, x_range=(-1.2, 1.2), y_range=(-1.2, 1.2),
+                      x_axis_location=None, y_axis_location=None, toolbar_location=None,
+                      title="Graph Interaction Demo", background_fill_color="#efefef",
+                      tooltips="index: @index, label: @label")
+        self.plot.grid.grid_line_color = None
+
+        self.graph_renderer = from_networkx(self.graph, nx.spring_layout, scale=1, center=(0, 0))
+        self.graph_renderer.node_renderer.glyph = Circle(size=15, fill_color="lightblue")
+        self.graph_renderer.edge_renderer.glyph = MultiLine(line_color="black",
+                                                       line_alpha=0.8, line_width=1.5)
+
+        self.layout.children[1] = self.plot
+
+        self.plot.renderers.append(self.graph_renderer)
+
+        curdoc().clear()
+        curdoc().add_root(self.layout)
 
     @staticmethod
     def _convert_graph(graph):
@@ -46,10 +64,10 @@ class Visualization:
         index = []
         label = []
 
-
-
     def _create_buttons(self):
         self.button.on_click(self.cos)
 
     def run(self):
+        curdoc().add_root(self.layout)
         show(self.layout)
+        # show(self.layout)
--- main.py
+++ main.py
@@ -4,19 +4,19 @@ from P2 import P2
 from P3 import P3
 from Visualization import Visualization
 
-if __name__ == '__main__':
-    graph = Graph()
-    graph.add_vertex('Z', {'source_type': 'apple'})
+# if __name__ == '__main__':
+graph = Graph()
+graph.add_vertex('Z', {'source_type': 'apple'})
 
-    P1.apply([graph.vertices[0]], graph)
-    print(graph)
+P1.apply([graph.vertices[0]], graph)
+print(graph)
 
-    P2.apply([graph.vertices[0], graph.vertices[1]], graph)
-    print(graph)
+P2.apply([graph.vertices[0], graph.vertices[1]], graph)
+print(graph)
 
-    graph.add_vertex('Z')
-    P3.apply([graph.vertices[0], graph.vertices[2]], graph)
-    print(graph)
+graph.add_vertex('Z')
+P3.apply([graph.vertices[0], graph.vertices[2]], graph)
+print(graph)
 
-    visualization = Visualization(graph)
-    visualization.run()
+visualization = Visualization(graph)
+visualization.run()
