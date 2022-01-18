81508817e0202815f9136675943c23896fc9b8c9
proman3419 <proman7268@gmail.com>
2022-01-18 10:35:11 +0100

    Changed the initial graph

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from Graph import Graph
 from Visualization import Visualization
 
-# if __name__ == '__main__':
+
 graph = Graph()
 graph.create_add_vertex('Z')
 graph.create_add_vertex('F')
+graph.create_add_vertex('P')
 graph.create_add_vertex('O')
 graph.create_add_edge('d', graph.vertices[0], graph.vertices[1])
 graph.create_add_edge('d', graph.vertices[1], graph.vertices[2])
+graph.create_add_edge('d', graph.vertices[2], graph.vertices[3])
+graph.create_add_edge('d', graph.vertices[0], graph.vertices[3])
 
 visualization = Visualization(graph)
 visualization.run()
